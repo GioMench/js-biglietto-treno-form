bcdb526dd2e2dee61297ff04a425cf7bd0dfe032
GioMench <giorgiaglm@gmail.com>
2024-02-20 17:23:02 +0100

    work on main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,27 @@
-console.log('test');
+//console.log('test');
+
+const forKm = 0.21
+
+const inputElementName = document.getElementById('name_surname');
+
+const inputElementKm = document.getElementById('km');
+
+const inputElementAge = document.getElementById('age');
+
+let price = inputElementKm * forKm
+
+document.getElementById('generate_btn').addEventListener('click', function () {
+    console.log(inputElementName.value, inputElementKm.value, inputElementAge.value);
+})
+
+
+if (inputElementAge < 18) {
+    const discountLess = 0.2
+    price = ((price - price * discountLess).toFixed(2));
+} else if (inputElementAge > 65) {
+    const discountOver = 0.4
+    price = ((price - price * discountOver).toFixed(2));
+}
+
+console.log(`${price}\u20AC`);
+
