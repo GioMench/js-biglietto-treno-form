40ab016ae25c379c5633324255bbbdcebf822895
GioMench <giorgiaglm@gmail.com>
2024-02-20 18:05:21 +0100

    set js for #generate_btn

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,42 @@
 //console.log('test');
 
 const forKm = 0.21
+//price for km
 
 const inputElementName = document.getElementById('name_surname');
+//input name of passenger
 
 const inputElementKm = document.getElementById('km');
+//input km 
 
 const inputElementAge = document.getElementById('age');
-
-let price = inputElementKm * forKm
+//input age of passenger
 
 document.getElementById('generate_btn').addEventListener('click', function () {
+    
     console.log(inputElementName.value, inputElementKm.value, inputElementAge.value);
+    //get value of input
+
+    let price = inputElementKm.value * forKm
+    //calc price standart
+
+    if (inputElementAge < 18) {
+        const discountLess = 0.2
+        price = ((price - price * discountLess).toFixed(2));
+        //calc price for less 18
+    } else if (inputElementAge > 65) {
+        const discountOver = 0.4
+        price = ((price - price * discountOver).toFixed(2));
+        //calc price for over 65
+    }
+    const finalPrice =`${price}\u20AC`
+
+    console.log(finalPrice);
+    //final price in human form
 })
 
 
-if (inputElementAge < 18) {
-    const discountLess = 0.2
-    price = ((price - price * discountLess).toFixed(2));
-} else if (inputElementAge > 65) {
-    const discountOver = 0.4
-    price = ((price - price * discountOver).toFixed(2));
-}
 
-console.log(`${price}\u20AC`);
+
+
 
